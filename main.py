ccf99da20087f8ea149c79b1735d8bd094d6f895
EnigmaK9 <enigmak9@protonmail.com>
2024-07-21 12:36:10 -0600

    animation added, now tiles are traversed

--- main.py
+++ main.py
@@ -28,52 +28,39 @@ def configure_matrices():
     matrix_random_paths_star = [row[:] for row in matrix_random]
 
 def execute_pathfinding():
-    global matrix_fixed_dijkstra, matrix_fixed_a_star, matrix_random_dijkstra, matrix_random_a_star
-    
+    global path_A1_fixed_dijkstra, path_A2_fixed_dijkstra, path_A1_fixed_a_star, path_A2_fixed_a_star
+    global path_A1_random_dijkstra, path_A2_random_dijkstra, path_A1_random_a_star, path_A2_random_a_star
+
     path_A1_fixed_dijkstra = Dijkstra(matrix_fixed, tuple(A1_start_pos), tuple(A1_goal_pos))
     path_A2_fixed_dijkstra = Dijkstra(matrix_fixed, tuple(A2_start_pos), tuple(A2_goal_pos))
-    
-    matrix_fixed_dijkstra = [row[:] for row in matrix_fixed]
-    mark_paths(matrix_fixed_dijkstra, path_A1_fixed_dijkstra, path_A2_fixed_dijkstra)
+
     path_lengths.append(len(path_A1_fixed_dijkstra) if path_A1_fixed_dijkstra else 0)
     path_lengths.append(len(path_A2_fixed_dijkstra) if path_A2_fixed_dijkstra else 0)
 
     path_A1_fixed_a_star = A_star(matrix_fixed_paths_star, tuple(A1_start_pos), tuple(A1_goal_pos))
     path_A2_fixed_a_star = A_star(matrix_fixed_paths_star, tuple(A2_start_pos), tuple(A2_goal_pos))
-    
-    matrix_fixed_a_star = [row[:] for row in matrix_fixed]
-    mark_paths(matrix_fixed_a_star, path_A1_fixed_a_star, path_A2_fixed_a_star)
+
     path_lengths.append(len(path_A1_fixed_a_star) if path_A1_fixed_a_star else 0)
     path_lengths.append(len(path_A2_fixed_a_star) if path_A2_fixed_a_star else 0)
 
     path_A1_random_dijkstra = Dijkstra(matrix_random, tuple(A1_start_pos), tuple(A1_goal_pos))
     path_A2_random_dijkstra = Dijkstra(matrix_random, tuple(A2_start_pos), tuple(A2_goal_pos))
-    
-    matrix_random_dijkstra = [row[:] for row in matrix_random]
-    mark_paths(matrix_random_dijkstra, path_A1_random_dijkstra, path_A2_random_dijkstra)
+
     path_lengths.append(len(path_A1_random_dijkstra) if path_A1_random_dijkstra else 0)
     path_lengths.append(len(path_A2_random_dijkstra) if path_A2_random_dijkstra else 0)
 
     path_A1_random_a_star = A_star(matrix_random_paths_star, tuple(A1_start_pos), tuple(A1_goal_pos))
     path_A2_random_a_star = A_star(matrix_random_paths_star, tuple(A2_start_pos), tuple(A2_goal_pos))
-    
-    matrix_random_a_star = [row[:] for row in matrix_random]
-    mark_paths(matrix_random_a_star, path_A1_random_a_star, path_A2_random_a_star)
+
     path_lengths.append(len(path_A1_random_a_star) if path_A1_random_a_star else 0)
     path_lengths.append(len(path_A2_random_a_star) if path_A2_random_a_star else 0)
 
 def display_results():
     if obstacle_type == "fixed":
-        print_path(matrix_fixed, "Fixed Obstacles")
-        print_path(matrix_fixed_dijkstra, "Fixed Obstacles Dijkstra")
-        print_path(matrix_fixed_a_star, "Fixed Obstacles A*")
-        draw_matrices(matrix_fixed, matrix_fixed_dijkstra, matrix_fixed_a_star, A1_start_pos, A2_start_pos, A1_goal_pos, A2_goal_pos, path_lengths[:4])
+        draw_matrices(matrix_fixed, A1_start_pos, A2_start_pos, A1_goal_pos, A2_goal_pos, path_A1_fixed_dijkstra, path_A2_fixed_dijkstra, path_A1_fixed_a_star, path_A2_fixed_a_star)
     else:
-        print_path(matrix_random, "Random Obstacles")
-        print_path(matrix_random_dijkstra, "Random Obstacles Dijkstra")
-        print_path(matrix_random_a_star, "Random Obstacles A*")
-        draw_matrices(matrix_random, matrix_random_dijkstra, matrix_random_a_star, A1_start_pos, A2_start_pos, A1_goal_pos, A2_goal_pos, path_lengths[4:])
-    
+        draw_matrices(matrix_random, A1_start_pos, A2_start_pos, A1_goal_pos, A2_goal_pos, path_A1_random_dijkstra, path_A2_random_dijkstra, path_A1_random_a_star, path_A2_random_a_star)
+
     show_results(path_lengths)
 
 if __name__ == "__main__":
